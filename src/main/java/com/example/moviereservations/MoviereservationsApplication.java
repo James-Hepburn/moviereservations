e1b39a2c08be2a20ef4fd151c6f54adaf103c080
James Hepburn <jameshepburn@Jamess-MacBook-Pro.local>
2025-09-02 12:19:00 -0600

    Initial commit

---
+++ src/main/java/com/example/moviereservations/MoviereservationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.moviereservations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviereservationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviereservationsApplication.class, args);
+	}
+
+}
